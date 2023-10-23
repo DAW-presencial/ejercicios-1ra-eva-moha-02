8026430a5d700175b40a5eca45b8924fbe8121d9
moha-02 <119495982+moha-02@users.noreply.github.com>
2023-10-23 15:57:55 +0200

    fin

--- formulario_agenda/agenda.php.php
+++ formulario_agenda/agenda.php.php
@@ -1,3 +1,28 @@
+<?php 
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+       
+        $agenda = array(); //array que será la agenda
+        
+        if (!empty($_POST['agenda'])) { //Comprueba si el campo agenda esta vacio tras enviarlo desde el POST
+            
+            $agenda = json_decode($_POST['agenda'], true); //pasa el contenido de la agenda desde JSON
+            $nombre = $_POST['nombre'];
+            $numero = $_POST['numero'];
+        }        
+        if( !empty($numero)) { //si el numero no esta vacio se asigna el numero al nombre o directamente se crea un nuevo contacto
+
+            $agenda[$nombre] = $numero;
+
+        }else{ // si el campo del numero esta vacio se elimina el contacto correspondiente al numero introducido
+
+            unset ($agenda [$nombre]);
+        }
+        //guarda en formato JSON la agenda de vuelta en el campo hidden para recuperarla posteriormente después del submit
+        $hiddenAgenda = htmlentities(json_encode(($agenda)));
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,56 +32,30 @@
 </head>
 <body>
 
-<form method="post" action="index.php">
+    <form method="post" action="index.php">
    
 
-    <label>Nombre:</label>
-    <input type="text" name="nombre" required>
-    <br>
-    <br>
-    <label>Telefono:</label>
-    <input type="number" name="numero" >
+        <label>Nombre:</label>
+        <input type="text" name="nombre" required>
+        <br>
+        <br>
+        <label>Telefono:</label>
+        <input type="number" name="numero" >
 
-    <input type="hidden" name="agenda" value="<?php echo $hiddenAgenda; ?>">
+        <input type="hidden" name="agenda" value="<?= $hiddenAgenda ?>">
 
-    <input type="submit" value="Submit">
-    <br>
-    <br>
-</form>
+        <input type="submit" value="Submit">
+        <br>
+        <br>
 
+        <ul>
+            <?php 
+           foreach ($agenda as $key => $value) { //bucle que permite imprimir el array de los contactos
+            echo "Nombre: {$key}; Tel: {$value}<br>";
+            } 
+            ?>
+        </ul>
+
+    </form>
 </body>
 </html>
-
-<?php 
-
-    if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-       
-        $hiddenAgenda = $_POST['agenda']; //recibe el string que contiene el  array que guarda la agenda
-        $explodedArray = explode(',', $hiddenAgenda); // convierte la agenda en array 
-       
-        $nombre = $_POST['nombre'];
-        $numero = $_POST['numero'];
- 
-        $agenda = array(); //array que será la agenda
-
-        foreach ($explodedArray as $pair) { // permite guardar en array associativo la agenda previamente recuperada
-            list($key, $value) = explode(':', $pair, 2);
-            $agenda[$key] = $value;
-        }
-
-        if( !empty($numero)) { //si el numero no esta vacio se asigna el numero al nombre o directamente se crea un nuevo contacto
-
-            $agenda[$nombre] = $numero;
-
-        }else{ // si el campo del numero esta vacio se elimina el contacto correspondiente al numero introducido
-
-            unset ($agenda [$nombre]);
-        }
-        foreach ($agenda as $key => $value) { //bucle que permite imprimir el array de los contactos
-            echo "Nombre: {$key}; Tel: {$value}";
-        } 
-
-        $hiddenAgenda = implode(',', $agenda); //guarda la agenda de vuelta en el campo hidden para recuperarla posteriormente después del submit
-    }
-
-?>
